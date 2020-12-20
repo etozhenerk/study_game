7a69082968cd01a9d7d2e906b56ad0232355f2e3
etozhenerk <etozhenerk@yandex.ru>
2020-12-20 13:41:26 +0300

    Усложненное задание

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-'use strict';
+"use strict";
 
-let isNumber = function(n){
+let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-
-function game(){
-  let question = prompt('Угадай число от 1 до 100');
-  let number = 10; //Загаданное число
-  let answer;
-  function getQuestion() { 
-    if(question === null){
-      answer = alert('Игра окончена');
-    }else if(isNumber(question)){
-      if(question > 100){
-        question = prompt('Загаданное число меньше');
-        getQuestion();
-      }else if(question < 1){
-        question = prompt('Загаданное число больше');
-        getQuestion();
-      }else if(question == number){
-        answer = alert('Поздравляю, Вы угадали!!!');
-      }else {
-        answer = alert('Вы не угадали');
+function game() {
+  let question = prompt("Угадай число от 1 до 100"),
+    number = 10, //Загаданное число
+    conter = 1,
+    answer,
+    win;
+  function getQuestion() {
+    if (question === null) {
+      answer = alert("Игра окончена");
+    } else if (isNumber(question)) {
+      if (conter < 10) {
+        if (question > 100) {
+          question = prompt(
+            `Загаданное число меньше, осталось попыток ${10 - conter}`
+          );
+          conter++;
+          getQuestion();
+        } else if (question < 1) {
+          question = prompt(
+            `Загаданное число больше, осталось попыток ${10 - conter}`
+          );
+          conter++;
+          getQuestion();
+        } else if (question == number) {
+          win = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+          if (win) {
+            game();
+          } else {
+            answer = alert("Пока!");
+          }
+        } else {
+          answer = alert("Вы не угадали");
+        }
+      } else {
+        win = confirm("Попытки закончились, хотите сыграть еще?");
+        if (win) {
+          game();
+        } else {
+          answer = alert("Пока!");
+        }
       }
-    }else{
-      question = prompt('Введи число!');
+    } else {
+      question = prompt("Введи число!");
       getQuestion();
     }
-   }
-   getQuestion();
+  }
+  getQuestion();
 }
+
 game();
